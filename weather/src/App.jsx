59db3d5bfc677baec27f1d178091acc0e13b50f5
Filip Andrejuk <filip_andrejuk@hotmail.com>
2025-01-04 17:59:15 -0500

    Text box takes location input for API call

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -7,19 +7,20 @@ function App() {
   const [count, setCount] = useState(0)
   const [location, setLocation] = useState("")
 
-  async function getData() {
-    let response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Toronto?unitGroup=metric&key=FFYXH369VAFUZNNC2L7V2GSKG&contentType=json')
+  async function getData(location) {
+    let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=FFYXH369VAFUZNNC2L7V2GSKG&contentType=json`)
     let responseJSON = await response.json();
     console.log(responseJSON)
   }
 
   function updateLocation(input) {
-    setLocation(input.value)
+    setLocation(input)
   }
 
   function updateCount() {
     setCount(count => count + 1);
     getData()
+    console.log(location)
   }
 
   return (
@@ -27,7 +28,7 @@ function App() {
     <p>
      Hello world! The count is {count}.
      </p>
-     <input onChange={(event) => console.log(event.target.value)}></input>
+     <input onChange={(event) => updateLocation(event.target.value)}></input>
      <p>{location}</p>
      <button onClick={updateCount}>Click me!</button>
     </>
