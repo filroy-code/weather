0321689c8b2014df1b17d19b07d70cb2e3451698
Filip Andrejuk <filip_andrejuk@hotmail.com>
2025-01-04 16:05:54 -0500

    API call is working

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -6,28 +6,23 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  async function getData() {
+    let response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Toronto?unitGroup=metric&key=FFYXH369VAFUZNNC2L7V2GSKG&contentType=json')
+    let responseJSON = await response.json();
+    console.log(responseJSON)
+  }
+
+  function updateCount() {
+    setCount(count => count + 1);
+    getData()
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <p>
+     Hello world! The count is {count}.
+     </p>
+     <button onClick={updateCount}>Click me!</button>
     </>
   )
 }
